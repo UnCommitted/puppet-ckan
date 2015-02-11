b148be09ed198fe4acd055271b1c423815514110
Simon Sellar <simon.sellar@ersa.edu.au>
2015-02-11 04:27:11 +0000

    Created ckan user and group

--- manifests/app_server.pp
+++ manifests/app_server.pp
@@ -1,5 +1,22 @@
 # Configures the CKAN App Server
-class ckan::app_server {
+class ckan::app_server (
+  $ckan_node_id,
+) {
+  
+  # Create the ckan user and groups
+  group { 'ckan_group':
+    name   => 'ckan',
+    ensure => 'present'
+  }
+
+  user { 'ckan_user':
+    name           => 'ckan',
+    shell          => '/usr/sbin/nologin',
+    home           => '/usr/lib/ckan',
+    managehome     => true,
+    gid            => 'ckan',
+    require        => Group['ckan_group'];
+  }
 }
 
 # vim: set shiftwidth=2 softtabstop=2 textwidth=0 wrapmargin=0 syntax=ruby:
