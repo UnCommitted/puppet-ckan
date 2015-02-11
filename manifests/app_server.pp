49416fbbd198c4a0f74d338cd2b5431f1dee593f
Simon Sellar <simon.sellar@ersa.edu.au>
2015-02-11 04:39:59 +0000

    Created directories.

--- manifests/app_server.pp
+++ manifests/app_server.pp
@@ -2,7 +2,7 @@
 class ckan::app_server (
   $ckan_node_id,
 ) {
-  
+
   # Create the ckan user and groups
   group { 'ckan_group':
     name   => 'ckan',
@@ -10,12 +10,50 @@ class ckan::app_server (
   }
 
   user { 'ckan_user':
-    name           => 'ckan',
-    shell          => '/usr/sbin/nologin',
-    home           => '/usr/lib/ckan',
-    managehome     => true,
-    gid            => 'ckan',
-    require        => Group['ckan_group'];
+    name       => 'ckan',
+    shell      => '/usr/sbin/nologin',
+    home       => '/usr/lib/ckan',
+    managehome => true,
+    gid        => 'ckan',
+    require    => Group['ckan_group'];
+  }
+
+  # Create directories
+  file {
+    [
+      '/etc/ckan',
+      '/var/lib/ckan'
+    ]:
+      ensure  => 'directory',
+      owner   => 'ckan',
+      group   => 'ckan',
+      mode    => '0755',
+      require => [
+        User['ckan_user'],
+        Group['ckan_group']
+      ];
+
+    "/etc/ckan/${ckan_node_id}":
+      ensure  => 'directory',
+      owner   => 'ckan',
+      group   => 'ckan',
+      mode    => '0755',
+      require => [
+        File['/etc/ckan'],
+        User['ckan_user'],
+        Group['ckan_group']
+      ];
+
+    "/var/lib/ckan/${ckan_node_id}":
+      ensure  => 'directory',
+      owner   => 'ckan',
+      group   => 'ckan',
+      mode    => '0755',
+      require => [
+        File['/var/lib/ckan'],
+        User['ckan_user'],
+        Group['ckan_group']
+      ];
   }
 }
 
